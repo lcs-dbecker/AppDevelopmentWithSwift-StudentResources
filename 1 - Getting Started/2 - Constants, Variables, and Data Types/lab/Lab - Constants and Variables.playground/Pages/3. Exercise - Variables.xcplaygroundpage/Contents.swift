8a5f1607525d0d55deccaf23ed648ab6a36d71d7
Dylan Becker <dbecker@lcs-mba3263.lcs.on.ca>
2017-12-11 09:36:46 -0500

    done with 3rd page

--- 1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/3. Exercise - Variables.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/3. Exercise - Variables.xcplaygroundpage/Contents.swift
@@ -3,16 +3,16 @@
  
  Declare a variable `schooling` and set it to the number of years of school that you have completed. Print `schooling` to the console.
  */
-
+var schooling = 10
 
 /*:
  Now imagine you just completed an additional year of school, and update the `schooling` variable accordingly. Print `schooling` to the console.
  */
-
+schooling = 11
 
 /*:
  Does the above code compile? Why is this different than trying to update a constant? Print your explanation to the console using the `print` function.
  */
-
+print("Yes, variables are able to change the return value")
 
 //: [Previous](@previous)  |  page 3 of 10  |  [Next: App Exercise - Step Count](@next)
